1a591ed2ab3a6dfb6b6091f0cb25e4b7d1139efc
amancapy <amankp1701@gmail.com>
2023-09-28 20:36:47 +0530

    final 8 tile

--- 8tile-a-star.py
+++ 8tile-a-star.py
@@ -4,15 +4,13 @@ import random
 action_index_change = {"U": -3, "D": +3, "L": -1, "R": +1}
 oppossite_actions = {"U": "D", "D": "U", "L": "R", "R": "L", "S": None}
 start_state = [1, 2, 3, 4, 5, 6, 7, 8, 0] # before shuffling
-
     
 h1 = lambda x: len([i == j for i, j in zip(x[0], [1, 2, 3, 4, 5, 6, 7, 8, 0]) if i == j]) # num_tiles out of place
 h2 = lambda x: -sum([abs(((x[0][i] - 1) % 9 - ((x[0][i] - 1) % 9) % 3) / 3 - (i - i % 3) / 3) + abs(((x[0][i] - 1) % 9) % 3 - i % 3) for i in range(9)]) / 2 # manhattan
-pick = h2
+pick = h1
 
 # a_star = heuristic * inadmissibility_factor + route_length
-a_star = lambda x: pick(x) * 100 - len(x[1])
-
+key = lambda x: pick(x) * 100 - len(x[1])
 
 for i in range(1000):
     # inversion parity test for whether random state is solvable.
@@ -23,13 +21,11 @@ for i in range(1000):
 
     print(i, start_state, end=" ")
 
-
-    queue = [(start_state, "S")] # "S" => starting
+    queue = [(start_state, "S", 0)] # "S" => starting
     visited = {} # remember hashing?
 
     while queue: # while queue is not empty
-        if len(queue) % 10 == 0:
-            queue.sort(key=a_star)
+        queue.sort(key=lambda x: x[2])
         # print(len(visited), a_star(queue[-1]))
 
         curr_state = queue.pop()
@@ -46,10 +42,10 @@ for i in range(1000):
                 temp_state[curr_zero_idx], temp_state[new_zero_idx] = temp_state[new_zero_idx], temp_state[curr_zero_idx] # a, b = b, a => switch the values of a and b
                 
                 if tuple(temp_state) not in visited or (tuple(temp_state) in visited and len(visited[tuple(temp_state)]) > len(curr_state[1]) + 1):
-                    queue.append((temp_state, curr_state[1] + drn))
+                    subtuple = (temp_state, curr_state[1] + drn)
+                    queue.append((*subtuple, key(subtuple)))
                 
                 if temp_state == [1, 2, 3, 4, 5, 6, 7, 8, 0]:
                     print(curr_state[1] + drn[1:], len(curr_state[1]), len(visited))
                     queue = []
                     break
-                    
